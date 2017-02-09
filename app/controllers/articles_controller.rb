c5c5d9c34e61f4e693ff2bea7b22d82e00a9141a
Rails Udemy Course <railsudemycourse@gmail.com>
2017-02-09 14:13:17 +0000

    added error handling , create and show function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,21 @@
 class ArticlesController < ApplicationController
     def new
-    @articles = Article.new
+    @article = Article.new
     end
     
     def create
-        #render plain: params[:article].inspect
         @article = Article.new(article_params)
-        @article.save
-        rederict_to_articles_show(@article)
+        if @article.save
+            flash[:notice] = "Article was successfully created"
+            redirect_to article_path(@article)
+            
+        else
+            render 'new'
+        end
+    end
+    
+    def show
+        @article = Article.find(params[:id])    
     end
     
     private
