5c06ba0b60814c9d39a654f616bba12fcd3093c9
Rails Udemy Course <railsudemycourse@gmail.com>
2017-03-15 13:38:20 +0000

    added associations between user and articles and added debugger

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    has_many :articles
     validates :username, presence: true, length: { minimum: 3, maximum: 21 }, uniqueness: { case_sensitive: false }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { minimum: 5, maximum: 35 },
