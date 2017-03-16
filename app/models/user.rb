927f77e609302c9ca5e201609b11ff08bad0f32d
Rails Udemy Course <railsudemycourse@gmail.com>
2017-03-16 02:32:56 +0000

    added secure password function to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
     validates :email, presence: true, length: { minimum: 5, maximum: 35 },
                 uniqueness: { case_sensitive: false }, 
                 format: { with: VALID_EMAIL_REGEX }
+                
+    has_secure_password
 end
