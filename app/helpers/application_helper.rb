c97930fbbb9e534683f72a6ee2341146091d3dac
Rails Udemy Course <railsudemycourse@gmail.com>
2017-03-22 06:56:41 +0000

    Added functionallity to User

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def gravatar_for(obj, options = {size: 80})
+        gravatar_id = Digest::MD5::hexdigest(obj.email.downcase)
+        size = options[:size]
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravatar_url, class: "img-circle", alt: obj.username)
+    end
 end
