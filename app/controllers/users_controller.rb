885c8ecc2a8e052616e1f97b31e2e81c2a952c76
Rails Udemy Course <railsudemycourse@gmail.com>
2017-03-23 09:18:27 +0000

    Added login, authorization, other user functionalities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-    
+    before_action :set_user,only: [ :edit, :update, :show ]
+    before_action :require_same_user, only: [:edit, :update ]
     def index
-        @user = User.paginate(page: params[:page], per_page: 3)
+        @user = User.paginate(page: params[:page], per_page: 5)
     end
     
     def new
@@ -11,19 +12,18 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to your account #{ @user.username }"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'new'
         end
     end
     
     def edit
-        @user = User.find(params[:id])
     end
     
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:success] = "Your profile has been updated"
             redirect_to articles_path
@@ -33,12 +33,22 @@ class UsersController < ApplicationController
     end
     
     def show
-        @user = User.find(params[:id])
         @user_paginate = @user.articles.paginate(page: params[:page], per_page: 3)
     end
     
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+    
+    def set_user
+        @user = User.find(params[:id])
+    end
+    
+    def require_same_user
+        if current_user != @user
+            flash[:danger] = "You can only edit your account"
+            redirect_to root_path
+        end
     end
 end
